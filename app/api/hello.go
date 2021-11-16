6575a65318ab0011be397a771e06fcdacdb8f7c9
thorne <thorne_lew@126.com>
2021-11-16 11:58:17 +0800

    删除dao

--- app/api/hello.go
+++ app/api/hello.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
 )
 
@@ -11,8 +10,5 @@ type helloApi struct{}
 
 // Index is a demonstration route handler for output "Hello World!".
 func (*helloApi) Index(r *ghttp.Request) {
-	db := g.DB()
-	// db.Model("user").Delete(1)
-	db.Model("user").Data(g.Map{"name": "小丽1", "age": "18"}).Insert()
 	r.Response.Writeln("Hello World!")
 }
