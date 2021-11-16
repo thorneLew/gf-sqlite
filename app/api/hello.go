72a3b2c35d54f82d50211ca27b0616ca8d94a830
thorne <thorne_lew@126.com>
2021-11-16 11:56:12 +0800

    db的使用

--- app/api/hello.go
+++ app/api/hello.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
 )
 
@@ -10,5 +11,8 @@ type helloApi struct{}
 
 // Index is a demonstration route handler for output "Hello World!".
 func (*helloApi) Index(r *ghttp.Request) {
+	db := g.DB()
+	// db.Model("user").Delete(1)
+	db.Model("user").Data(g.Map{"name": "小丽1", "age": "18"}).Insert()
 	r.Response.Writeln("Hello World!")
 }
